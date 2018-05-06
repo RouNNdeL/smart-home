a91ab382256ca0c8df7bf710ab47945b199dc54b
RouNdeL <krzys.zdulski@gmail.com>
2018-05-06 20:22:40 +0200

    Support multiple 'modes', do not send content on check

--- api/esp_update.php
+++ api/esp_update.php
@@ -13,7 +13,7 @@ if(!isset(apache_request_headers()["x-ESP8266-version"]) || !ctype_digit(apache_
     exit(0);
 }
 $device_id = $_GET["device_id"];
-$version = (int) apache_request_headers()["x-ESP8266-version"];
+$version = (int)apache_request_headers()["x-ESP8266-version"];
 $md5 = apache_request_headers()["x-ESP8266-sketch-md5"];
 $newest_file = null;
 $newest_version = -1;
@@ -21,20 +21,34 @@ $dir = __DIR__ . "/../iot_binaries/" . $device_id . "/";
 foreach(scandir($dir) as $item)
 {
     preg_match("/bin_(\d+)\.bin/", $item, $matches);
-    if(isset($matches[1]) && (int) $matches[1] > $newest_version)
+    if(isset($matches[1]) && (int)$matches[1] > $newest_version)
     {
         $newest_file = $matches[0];
-        $newest_version = (int) $matches[1];
+        $newest_version = (int)$matches[1];
     }
 }
 $filename = $dir . $newest_file;
-if($newest_version !== null &&  ($newest_version >  $version ||
+if($newest_version !== null && ($newest_version > $version ||
         ($newest_version === $version && $md5 !== md5_file($filename))))
 {
-    header("Content-Type: application/octet-stream");
-    header("Content-Length: ".filesize($filename));
-    header("x-MD5: ".md5_file($filename));
-    readfile($filename);
+    switch(apache_request_headers()["x-ESP8266-mode"])
+    {
+        case "sketch":
+            http_response_code(200);
+            header("Content-Type: application/octet-stream");
+            header("Content-Length: " . filesize($filename));
+            header("x-MD5: " . md5_file($filename));
+            readfile($filename);
+            break;
+        case "check":
+            http_response_code(204);
+            break;
+        case "spiffs":
+            http_response_code(501);
+            break;
+        default:
+            http_response_code(400);
+    }
     exit(0);
 }
 
