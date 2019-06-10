384957b8b3eca12d918c285cb37d2b9c300009bb
RouNdeL <krzys.zdulski@gmail.com>
2019-06-10 13:46:56 +0200

    Change directory before executing the script

--- scripts/download_jqueryui.sh
+++ scripts/download_jqueryui.sh
@@ -1,4 +1,8 @@
 #!/usr/bin/env bash
+parent_path=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
+
+cd "$parent_path"
+
 mkdir -p ../lib
 cd ../lib
 wget https://jqueryui.com/resources/download/jquery-ui-1.12.1.zip
